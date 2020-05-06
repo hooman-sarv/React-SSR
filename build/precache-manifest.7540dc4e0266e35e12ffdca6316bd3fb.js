13a0913d56e72b12e35415061b99ffa9fbc755c3
Hooamn_Sarv <hoomansarvghaddiiut@gmail.com>
2020-05-06 16:25:42 +0430

    final commit

---
+++ build/precache-manifest.7540dc4e0266e35e12ffdca6316bd3fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81dd7fa5e4e036ad8fcf7556d661497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6958ac677bd0c5587228",
+    "url": "/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "5e347a4ef6e31efc2a00",
+    "url": "/static/js/2.27c0b9b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27c0b9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6958ac677bd0c5587228",
+    "url": "/static/js/main.7f5c101c.chunk.js"
+  },
+  {
+    "revision": "83ad52f628dd4978943e",
+    "url": "/static/js/runtime-main.40afb558.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useState}from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+    
+  const Inc = () =>{
+      setCount(count + 1)
+  }
+  const dec = () =>{
+      setCount(count - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className ='App'>
+          <p>{count}</p>
+          <button onClick={Inc} >Increment</button>
+          <button onClick={dec} >Decrement</button>
+      </div>
+  )
+
 }
 
 export default App;
